8de1f255f19cd4b292b6e0c9d657ed28da5618f3
Jose Manuel <jmdcarq89@gmail.com>
2021-04-23 12:24:17 +0200

    mongoose connection

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hola mundo");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: ".env" });
+
+mongoose.connect(
+  process.env.BBDD,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  },
+  (err, _) => {
+    if (err) {
+      console.log("Error de conexion");
+    } else {
+      console.log("Conexion correcta");
+    }
+  }
+);

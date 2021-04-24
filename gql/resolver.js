12f6a1274ba5d3d2d9fc2f48e6538804be819097
Jose Manuel <jmdcarq89@gmail.com>
2021-04-24 18:03:05 +0200

    password encription

--- gql/resolver.js
+++ gql/resolver.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
 
 const resolvers = {
   Query: {
@@ -27,7 +28,8 @@ const resolvers = {
       if (foundUsername) throw new Error("El nombre de usuario ya esta en uso");
 
       //Encriptar.
-      //....
+      const salt = await bcryptjs.genSaltSync(10);
+      newUser.password = await bcryptjs.hash(password, salt);
 
       try {
         const user = new User(newUser);

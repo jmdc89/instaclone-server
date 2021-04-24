2054a65576408c6daf622fbde91d2f1b19797393
Jose Manuel <jmdcarq89@gmail.com>
2021-04-24 17:52:07 +0200

    save users

--- gql/resolver.js
+++ gql/resolver.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 const resolvers = {
   Query: {
     // user
@@ -9,9 +11,31 @@ const resolvers = {
 
   Mutation: {
     //User
-    register: (_, { input }) => {
-      console.log("Registrando usuarios");
-      return null;
+    register: async (_, { input }) => {
+      const newUser = input;
+      newUser.email = newUser.email.toLowerCase();
+      newUser.username = newUser.username.toLowerCase();
+
+      const { email, username, password } = newUser;
+
+      //Revisamos si el email esta en uso
+      const foundEmail = await User.findOne({ email });
+      if (foundEmail) throw new Error("El email ya esta en uso");
+
+      //Revisamos si el username esta en uso
+      const foundUsername = await User.findOne({ username });
+      if (foundUsername) throw new Error("El nombre de usuario ya esta en uso");
+
+      //Encriptar.
+      //....
+
+      try {
+        const user = new User(newUser);
+        user.save();
+        return user;
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };

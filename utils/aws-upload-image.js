7531ff5ca654476dc661aeef5b7f1bd9a02c3f00
Jose Manuel <jmdcarq89@gmail.com>
2021-04-28 11:59:58 +0200

    uploadimage aws

--- utils/aws-upload-image.js
+++ utils/aws-upload-image.js
@@ -9,3 +9,21 @@ const s3 = new AWS.S3({
   accessKeyId: ID,
   secretAccessKey: SECRET,
 });
+
+async function awsUploadImage(file, filePath) {
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: `${filePath}`,
+    Body: file,
+  };
+
+  try {
+    const response = await s3.upload(params).promise();
+    return response.Location;
+  } catch (error) {
+    console.log(error);
+    throw new Error();
+  }
+}
+
+module.exports = awsUploadImage;
